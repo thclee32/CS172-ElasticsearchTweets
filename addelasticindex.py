bacf3c2b5c618c140ec5a0a8f944cadddb20738f
mrthomastreelee <thclee32@gmail.com>
2019-03-12 13:58:31 -0700

    changed addelasticindex.py, put data into rawtweets and elasticreadytweets

--- addelasticindex.py
+++ addelasticindex.py
@@ -1,22 +1,26 @@
 import json
 
-inputfile = open('twitter_all.txt', 'r')
-outputfile = open('elasticreadytweets.txt', 'w')
-
-
+filecount = 0
 count = 1
+for filecount in range(11):
+    filestring = str(filecount)
+    inputfile = open('rawtweets/twitter_data' + filestring  + '.txt', 'r')
+    outputfile = open('elasticreadytweets/elastictwitter_data' + filestring + '.txt', 'w')
 
-#cnt = str(count)
-#outputfile.write('''{"index":{"_index":"tweetindex", "_type":"tweet", "_id": "''' + cnt + '''"}}''')
 
 
-for line in inputfile:
-    cnt = str(count)
-    if line != "\n":
-        outputfile.write('''{"index":{"_index":"tweetindex", "_type":"tweet", "_id": "''' + cnt + '''"}}\n''')
-        outputfile.write(line)
-        count += 1
-    
+    #cnt = str(count)
+    #outputfile.write('''{"index":{"_index":"tweetindex", "_type":"tweet", "_id": "''' + cnt + '''"}}''')
 
-inputfile.close()
-outputfile.close()
+
+    for line in inputfile:
+        cnt = str(count)
+        if line != "\n":
+            outputfile.write('''{"index":{"_index":"tweetindex", "_type":"tweet", "_id": "''' + cnt + '''"}}\n''')
+            outputfile.write(line)
+            count += 1
+        
+
+    inputfile.close()
+    outputfile.close()
+    filecount += 1
